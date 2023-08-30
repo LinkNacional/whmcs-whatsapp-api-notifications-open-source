71bd0b519759038c05571c78a72ec69d7bbbdc26
Bruno Ferreira <ferreira.bruno@linknacional.com>
2023-08-30 14:34:41 +0000

    refactor: add tests file

---
+++ src/modules/addons/lknhooknotification/tests.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Access this file in your browser to test the notification you are working on.
+ *
+ * Use Messenger::runNow to simulate the hook params passed to an notification when WHMCS call it.
+ *
+ * You can access this file by adding /modules/addons/lknhooknotification/tests.php to your URL of your WHMCS
+ *
+ * @since 3.2.0.
+ */
+
+use Lkn\HookNotification\Domains\Notifications\Messenger;
+
+require_once __DIR__ . '/../../../init.php';
+
+$currentUser = new \WHMCS\Authentication\CurrentUser();
+
+if (!$currentUser->admin()) {
+    exit;
+}
