8442cc65eba04286948ddf13916aeb7b8c73b2a9
Bruno Ferreira <ferreira.bruno@linknacional.com>
2024-01-25 21:09:47 +0000

    feat(wp): #8 new ServiceSuspendedFor45DaysNotification

---
+++ src/modules/addons/lknhooknotification/src/Notifications/WhatsApp/ServiceSuspendedFor45Days/lang/portuguese-pt.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['notification_title'] = 'Serviço suspenso há 45 dias';
+$lang['notification_description'] = 'Roda no DailyCronJob.';
+
+// Nomes dos parâmetros
+
+$lang['service_id'] = 'ID do serviço';
+$lang['client_first_name'] = 'Primeiro nome do cliente';
+$lang['client_full_name'] = 'Nome completo do cliente';
+
+return $lang;
