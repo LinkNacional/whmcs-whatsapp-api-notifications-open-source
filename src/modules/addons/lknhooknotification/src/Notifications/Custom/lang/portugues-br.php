e292450912df022c06cdfedecae46223b901e682
Anacleto Ramon <martins.anacleto@linknacional.com>
2025-06-18 08:59:50 -0300

    fix: tradução para portugues do nome das notificações

--- src/modules/addons/lknhooknotification/src/Notifications/Custom/lang/portugues-br.php
+++ src/modules/addons/lknhooknotification/src/Notifications/Custom/lang/portugues-br.php
@@ -2,3 +2,9 @@
 
 /**@var array<string, string> $_ADDONLANG */
 
+$_ADDONLANG['BirthdayClientNotification'] = 'Aniversario do cliente';
+$_ADDONLANG['ClientPasswordChangeConfirmed'] = 'Confirmação de alteração de senha do cliente';
+$_ADDONLANG['QuoteStatusChange'] = 'Atualização de status de orçamento';
+$_ADDONLANG['ServiceCheckAfter3Months'] = 'Checagem após 3 meses de serviço';
+$_ADDONLANG['UserLoginNotification'] = 'Login de usuario';
+$_ADDONLANG['UserPasswordChangeConfirmed'] = 'Confirmação de alteração na senha do usuario';
