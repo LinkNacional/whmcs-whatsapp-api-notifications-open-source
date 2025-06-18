17578024b8a6f27c84cdc203ab95413a06a2f5e7
Anacleto Ramon <martins.anacleto@linknacional.com>
2025-06-18 09:42:40 -0300

    feat: Notificaçãode status de orçamento para aceito

--- src/modules/addons/lknhooknotification/src/Notifications/Custom/lang/portugues-br.php
+++ src/modules/addons/lknhooknotification/src/Notifications/Custom/lang/portugues-br.php
@@ -4,7 +4,8 @@
 
 $_ADDONLANG['BirthdayClientNotification'] = 'Aniversario do cliente';
 $_ADDONLANG['ClientPasswordChangeConfirmed'] = 'Confirmação de alteração de senha do cliente';
-$_ADDONLANG['QuoteStatusChange'] = 'Atualização de status de orçamento';
+$_ADDONLANG['QuoteStatusChange'] = 'Atualização de status de orçamento para perdido ou morto';
 $_ADDONLANG['ServiceCheckAfter3Months'] = 'Checagem após 3 meses de serviço';
 $_ADDONLANG['UserLoginNotification'] = 'Login de usuario';
 $_ADDONLANG['UserPasswordChangeConfirmed'] = 'Confirmação de alteração na senha do usuario';
+$_ADDONLANG['QuoteStatusAccepted'] = 'Atualização de status de orçamento para aceito';
